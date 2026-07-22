b14fda698010bbe6fd21f510d860e13e241fb6b3
Lokesh Das <thelokesh08@gmail.com>
2026-07-21 22:20:58 -0500

    minor adjustment to current change

--- data_generation/run_data_gen_pipe.sh
+++ data_generation/run_data_gen_pipe.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
-
 sudo -v
 
 echo "Running the Data Generation pipeline"
+python3 gen_connection.py > ./data/target_logs.txt 2>&1 &
 
-python3 target.py > target_logs.txt 2>&1 &
 TARGET_PID=$!
-
 echo "Target launched with PID: $TARGET_PID"
 
-sudo python3 hook_network_info.py --pid "$TARGET_PID"
+sudo python3 hook_network.py --pid "$TARGET_PID"
