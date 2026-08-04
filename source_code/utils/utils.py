db12ccc344f6ed0f82277ce49c929e6c727ae4dd
Lokesh Das <thelokesh08@gmail.com>
2026-08-03 23:40:53 -0500

    Model Loading pipeline

---
+++ source_code/utils/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def load_data(csv_path: str, view_head=False):
+    try:
+        data = pd.read_csv(csv_path)
+        if data is not None:
+            if view_head:
+                print(f"\n{'='* 100}\n")
+                print(data.head(5))
+                print(f"\n{'='* 100}\n")
+                print(data.tail(5), "\n")
+            return data
+        else:
+            print("Nothing in csv file")
+            return None
+    except:
+        raise FileNotFoundError("Faulty CSV path")
