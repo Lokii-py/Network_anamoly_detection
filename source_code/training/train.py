f463327a481c0c406197fe8ff87da8471e238763
Lokesh Das <thelokesh08@gmail.com>
2026-08-12 23:12:36 -0500

    Removing the test block and adding average accuracy

---
+++ source_code/training/train.py
@@ -0,0 +1,42 @@
+import logging
+
+from source_code.data_processing.process_data import preprocess_data
+from source_code.training.back_propagation import compute_weight_gradient, compute_bias_gradient
+from source_code.training.loss import loss
+from source_code.training.optimizer import update_weight, update_bias
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] %(message)s'
+)
+
+def train_model(csv_path: str, model, epochs=10, lr=0.1):
+    features, labels = preprocess_data(csv_path)
+
+    for epoch in range(epochs):
+        epoch_loss = 0.0
+
+        for x, y in zip(features, labels):
+            prediction = model.forward(x)
+            loss_value = loss(prediction, y)
+            
+            logging.debug(f"Actual Class: {y} | Prediction: {prediction:.4f} | Loss: {loss_value:.4f}")
+
+            for idx, feature in enumerate(x):
+                logging.debug(f"Model Weights_{idx} Before : {model.weights[idx]}")
+                weight_val = compute_weight_gradient(prediction, y, feature)
+
+                logging.debug(f"new weight computed gradient: {weight_val}")
+
+                update_weight(weight_val, lr, model, idx)
+                logging.debug(f"Model Weights_{idx} After : {model.weights[idx]}")
+
+            logging.debug(f"Model Bias Before: {model.bias}")
+            bias_val = compute_bias_gradient(prediction, y)
+
+            update_bias(bias_val, lr, model)
+            logging.debug(f"Model Bias After: {model.bias}")
+
+            epoch_loss += loss_value
+
+        logging.info(f"Epoch: {epoch+1} | Avg.loss: {epoch_loss:.4f}")
