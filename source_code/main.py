0cdb41789bf52b163465818888d827bb81c1ec27
Lokesh Das <thelokesh08@gmail.com>
2026-08-15 00:45:04 -0500

    Adding Validation pipeline for the model

--- source_code/main.py
+++ source_code/main.py
@@ -1,8 +1,20 @@
 from source_code.model.model import Model
-from source_code.training.train import train_model
+from source_code.training.train import train_model, evaluate_model
+from source_code.data_processing.process_data import preprocess_data
+from source_code.utils.utils import split_dataset
 
+EPOCHS = 100
+CSV_PATH = "data/data.csv"
 
 if __name__ == "__main__":
-
+    
     model = Model(num_weights=2)
-    train_model("data/data.csv", model, epochs=100)
+
+    features, labels = preprocess_data(CSV_PATH)
+    trainset, train_label, testset, test_label = split_dataset(features, labels)
+
+    for epoch in range(EPOCHS):
+        train_loss, model = train_model(trainset, train_label, model)
+        accuracy, test_loss = evaluate_model(testset, test_label, model)
+
+        print(f"Epoch: {epoch+1} | Train Loss: {train_loss:.4f} | Validation Loss: {test_loss:.4f} | Accuracy: {accuracy:.2f}")
--- source_code/training/train.py
+++ source_code/training/train.py
@@ -1,7 +1,6 @@
 import logging
 
 from source_code.training.loss import loss
-from source_code.data_processing.process_data import preprocess_data
 from source_code.training.optimizer import update_weight, update_bias
 from source_code.utils.utils import Accuracy
 from source_code.training.back_propagation import compute_weight_gradient, compute_bias_gradient
@@ -11,38 +10,55 @@ logging.basicConfig(
     format='[%(levelname)s] %(message)s'
 )
 
-def train_model(csv_path: str, model, epochs=10, lr=0.1):
-    features, labels = preprocess_data(csv_path)
+def train_model(trainset, train_label, model, lr=0.1):
+    full_loss = 0.0
+    full_acc = 0.0
 
-    for epoch in range(epochs):
-        epoch_loss = 0.0
-        epoch_acc = 0
+    for x, y in zip(trainset, train_label):
+        prediction = model.forward(x)
+        loss_value = loss(prediction, y)
+        # acc = Accuracy(prediction, y)
+        
+        logging.debug(f"Actual Class: {y} | Prediction: {prediction:.4f} | Loss: {loss_value:.4f}")
 
-        for x, y in zip(features, labels):
-            prediction = model.forward(x)
-            loss_value = loss(prediction, y)
-            acc = Accuracy(prediction, y)
-            
-            logging.debug(f"Actual Class: {y} | Prediction: {prediction:.4f} | Loss: {loss_value:.4f}")
+        for idx, feature in enumerate(x):
+            logging.debug(f"Model Weights_{idx} Before : {model.weights[idx]}")
+            weight_val = compute_weight_gradient(prediction, y, feature)
 
-            for idx, feature in enumerate(x):
-                logging.debug(f"Model Weights_{idx} Before : {model.weights[idx]}")
-                weight_val = compute_weight_gradient(prediction, y, feature)
+            logging.debug(f"new weight computed gradient: {weight_val}")
 
-                logging.debug(f"new weight computed gradient: {weight_val}")
+            update_weight(weight_val, lr, model, idx)
+            logging.debug(f"Model Weights_{idx} After : {model.weights[idx]}")
 
-                update_weight(weight_val, lr, model, idx)
-                logging.debug(f"Model Weights_{idx} After : {model.weights[idx]}")
+        logging.debug(f"Model Bias Before: {model.bias}")
+        bias_val = compute_bias_gradient(prediction, y)
 
-            logging.debug(f"Model Bias Before: {model.bias}")
-            bias_val = compute_bias_gradient(prediction, y)
+        update_bias(bias_val, lr, model)
+        logging.debug(f"Model Bias After: {model.bias}")
 
-            update_bias(bias_val, lr, model)
-            logging.debug(f"Model Bias After: {model.bias}")
+        full_loss += loss_value
+        # full_acc += acc
 
-            epoch_loss += loss_value
-            epoch_acc += acc
+    # full_loss /= len(train_label)
+    full_loss /= len(train_label)
 
-        epoch_loss /= len(features)
-        epoch_acc /= len(features)
-        logging.info(f"Epoch: {epoch+1} | Avg.loss: {epoch_loss:.4f} | Accuracy: {epoch_acc:.2f}")
+    return full_loss, model
+
+
+def evaluate_model(test_feature, test_labels, model):
+    full_pass_loss = 0.0
+    full_pass_acc = 0.0
+
+    for x, y in zip(test_feature, test_labels):
+        predictions = model.forward(x) 
+        acc = Accuracy(predictions, y)
+        loss_value = loss(predictions, y)
+
+        full_pass_acc += acc
+        full_pass_loss += loss_value
+
+    full_pass_acc /= len(test_labels)
+    full_pass_loss /= len(test_labels)
+
+    return full_pass_acc, full_pass_loss
+
